ea3a1c3d3fb52fe8019eebfae2134fec88183e84
c-degni <christivanndeg@gmail.com>
2025-08-17 18:43:47 -0400

    Remove dist/ (build artifacts)

--- packages/visualizer_component/dist/packages/visualizer_component/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as ExecutionPlayer } from '../../../src/components/shared/ExecutionPlayer';
-export { default as StackVisualizer } from '../../../src/components/visualizers/data_structures/StackVisualizer';
-export { default as VisualizationContainer } from './VisualizationContainer';
--- packages/visualizer_component/dist/src/components/visualizers/data_structures/StackVisualizer.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface StackVisualizerProps {
-    elements: any[];
-    highlights?: number[];
-}
-export default function StackVisualizer({ elements, highlights }: StackVisualizerProps): React.JSX.Element;
-export {};
