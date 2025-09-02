7db150ac532465d81a14602aa0ecb18696243a9b
c-degni <christivanndeg@gmail.com>
2025-09-02 15:28:27 -0400

    Package version bump and fixed bt traversals

--- packages/visualizer_component/src/index.ts
+++ packages/visualizer_component/src/index.ts
@@ -2,4 +2,5 @@ export { default as ExecutionPlayer } from '../../../src/components/shared/Execu
 export { default as StackVisualizer } from '../../../src/components/visualizers/data_structures/StackVisualizer';
 export { default as QueueVisualizer } from '../../../src/components/visualizers/data_structures/QueueVisualizer';
 export { default as LinkedListVisualizer } from '../../../src/components/visualizers/data_structures/LinkedListVisualizer';
+export { default as BinaryTreeVisualizer } from '../../../src/components/visualizers/data_structures/BinaryTreeVisualizer';
 export { default as VisualizationContainer } from './VisualizationContainer';
