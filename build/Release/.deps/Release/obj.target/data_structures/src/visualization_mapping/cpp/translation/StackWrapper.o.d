b734c01d99185b19cc20a6df6e510abe5c2858f5
c-degni <christivanndeg@gmail.com>
2025-09-07 17:19:04 -0400

    try2

---
+++ build/Release/.deps/Release/obj.target/data_structures/src/visualization_mapping/cpp/translation/StackWrapper.o.d
@@ -0,0 +1,24 @@
+cmd_Release/obj.target/data_structures/src/visualization_mapping/cpp/translation/StackWrapper.o := c++ -o Release/obj.target/data_structures/src/visualization_mapping/cpp/translation/StackWrapper.o ../src/visualization_mapping/cpp/translation/StackWrapper.cpp '-DNODE_GYP_MODULE_NAME=data_structures' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_DISABLE_CPP_EXCEPTIONS=1' '-DBUILDING_NODE_EXTENSION' -I/Users/christ/Library/Caches/node-gyp/23.8.0/include/node -I/Users/christ/Library/Caches/node-gyp/23.8.0/src -I/Users/christ/Library/Caches/node-gyp/23.8.0/deps/openssl/config -I/Users/christ/Library/Caches/node-gyp/23.8.0/deps/openssl/openssl/include -I/Users/christ/Library/Caches/node-gyp/23.8.0/deps/uv/include -I/Users/christ/Library/Caches/node-gyp/23.8.0/deps/zlib -I/Users/christ/Library/Caches/node-gyp/23.8.0/deps/v8/include -I/Users/christ/Documents/algo-vis/node_modules/node-addon-api -I../src/visualization_mapping/cpp -I../src/visualization_mapping/cpp/data_structures -I../src/visualization_mapping/cpp/core  -O3 -gdwarf-2 -fno-strict-aliasing -mmacosx-version-min=11.0 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=c++17 -stdlib=libc++ -fno-rtti -fno-exceptions -std=c++17 -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/data_structures/src/visualization_mapping/cpp/translation/StackWrapper.o.d.raw   -c
+Release/obj.target/data_structures/src/visualization_mapping/cpp/translation/StackWrapper.o: \
+  ../src/visualization_mapping/cpp/translation/StackWrapper.cpp \
+  /Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi.h \
+  /Users/christ/Library/Caches/node-gyp/23.8.0/include/node/node_api.h \
+  /Users/christ/Library/Caches/node-gyp/23.8.0/include/node/js_native_api.h \
+  /Users/christ/Library/Caches/node-gyp/23.8.0/include/node/js_native_api_types.h \
+  /Users/christ/Library/Caches/node-gyp/23.8.0/include/node/node_api_types.h \
+  /Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi-inl.h \
+  /Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../src/visualization_mapping/cpp/data_structures/TrackedStack.h \
+  ../src/visualization_mapping/cpp/data_structures/../core/ExecutionTracer.h \
+  ../src/visualization_mapping/cpp/data_structures/../core/json.hpp
+../src/visualization_mapping/cpp/translation/StackWrapper.cpp:
+/Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi.h:
+/Users/christ/Library/Caches/node-gyp/23.8.0/include/node/node_api.h:
+/Users/christ/Library/Caches/node-gyp/23.8.0/include/node/js_native_api.h:
+/Users/christ/Library/Caches/node-gyp/23.8.0/include/node/js_native_api_types.h:
+/Users/christ/Library/Caches/node-gyp/23.8.0/include/node/node_api_types.h:
+/Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi-inl.h:
+/Users/christ/Documents/algo-vis/node_modules/node-addon-api/napi-inl.deprecated.h:
+../src/visualization_mapping/cpp/data_structures/TrackedStack.h:
+../src/visualization_mapping/cpp/data_structures/../core/ExecutionTracer.h:
+../src/visualization_mapping/cpp/data_structures/../core/json.hpp:
